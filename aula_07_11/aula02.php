111a0391ba929657b12320a58c90da0822543981
jessicanjos <jessicanjos2003@gmail.com>
2022-11-07 20:21:22 -0300

    atualização

---
+++ aula_07_11/aula02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>FOrmulário - Recebendo dados - POST</title>
+</head>
+<body>
+	<h2>Formulário - Recebendo dados - POST</h2>
+
+	<form>
+		
+		Login:
+		<input type="text" name="nome"><br><br>
+		Senha:
+		<input type="password" name="senha"><br><br>
+
+		<input type="submit" name="enviar">
+
+	</form>
+</body>
+</html>
---
+++ aula_07_11/aula03.php
@@ -0,0 +1,12 @@
+<?php 
+	echo"<h3>Recebendo dados - POST<h/3>";
+	$nome = $_POST['nome'];
+	$senha = $_POST['senha'];
+
+
+	echo "<b>Nome: </br".$nome;
+	echo "<br/>";
+	echo "<b>Senha : </b>".$senha;
+	echo "<br/>";
+
+ ?>
